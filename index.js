0cc925223a677539dae30ca85a6c2317245e8716
Shakvilla <abdulshakuraclement@yahoo.com>
2022-03-02 14:47:26 +0000

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello World')
+const http = require('http')
+
+const requestHandler = function(req, res){
+
+     res.writeHead(200, {'Content-Type': 'text/plain'})
+     res.end('Hello World');
+
+}
+
+const server = http.createServer(requestHandler)
+server.listen(8080);
